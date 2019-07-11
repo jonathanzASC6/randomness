0f4fa52b61408adf6422f51ffa9b801ad3b38761
jonathanzASC6 <jzurita456@gmail.com>
2019-07-11 14:30:00 -0400

    randomness

---
+++ randomness.js
@@ -0,0 +1,15 @@
+// function randomNumber() {
+// const number = Math.random()*21 
+// const final = Math.floor(number)
+
+// return final
+// }
+
+// console.log(randomNumber());
+const letters = ['a', "b","c","d","e","f","g","h","i","j","k","l","m","n","o","P"]
+function randomLetter(){
+    const Letter = Math.random()*26
+    const final = Math.floor(Letter)()   
+    return final 
+}
+console.log(randomLetter())
